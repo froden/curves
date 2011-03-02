79031c0c1f568d9cb77ef91a4a6a921cc0b618c8
Frode Nerbråten <frode@nerbraten.no>
2011-03-02 07:55:28 +0100

    Added test to BSpline

--- src/test/scala/no/nerbraten/curves/gfx/BSplineFunctionalSuite.scala
+++ src/test/scala/no/nerbraten/curves/gfx/BSplineFunctionalSuite.scala
@@ -3,9 +3,21 @@ package no.nerbraten.curves.gfx
 import org.scalatest.FunSuite
 
 class BSplineFunctionalSuite extends FunSuite {
-  test("should work") {
+
+  test("Should calculate the points on the spline based on the given control points and resolution") {
     val points = List(new Point(0, 0), new Point(10, 0), new Point(10, 10), new Point(0, 10))
     val bspline = new BSpline(points, 10)
-    println(bspline.spline)
+    val ps = bspline.spline
+    //println(ps)
+    ps(0) === new Point(9, 2)
+    ps(1) === new Point(9, 2)
+    ps(2) === new Point(9, 3)
+    ps(3) === new Point(9, 3)
+    ps(4) === new Point(9, 4)
+    ps(5) === new Point(10, 5)
+    ps(6) === new Point(9, 5)
+    ps(7) === new Point(9, 7)
+    ps(8) === new Point(9, 7)
+    ps(9) === new Point(9, 8)
   }
 }
